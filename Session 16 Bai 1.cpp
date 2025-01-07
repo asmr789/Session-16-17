2edcfe2be8e6eb2b8b703e34c964a139b41d1c4b
phucdt <dotienphuc1607@gmail.com>
2025-01-07 13:38:50 +0700

    Upload

---
+++ Session 16 Bai 1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    int x = 10;
+    int *ptr = &x;      
+    printf("Gia tri cua x:%d\n",x);
+    printf("Dia chi cua x:%d\n",&x);
+    printf("Gia tri cua x:%d\n",*ptr);
+    printf("Dia chi cua x:%d\n",&ptr);
+
+    return 0;
+}
